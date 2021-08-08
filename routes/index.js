4e6ef872784e3ceb15f80dc43edd78f8ff87acdd
ray0103a <miel0103@yahoo.co.jp>
2021-08-08 17:58:37 +0900

    メインメニュー仮

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,32 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'TopPage' , message: 'ログインしてください'});
+/* TopPage */
+let isLogined = function(req, res, next){
+    console.log(req.user);
+    if(req.isAuthenticated()){
+        if (req.user.myKey == 'zzz') {
+            //ログインOKの場合(router.getの中が処理される)
+            return next();
+        } else {
+            //権限がない場合
+            res.redirect("/");
+        }
+    }else{
+        //認証されていない場合
+        res.render('index', { title: 'TopPage' , message: 'LOGIN'});
+    }
+};
+
+router.get('/', isLogined, function(req, res, next) {
+    //認証が既にされている場合は、メインメニューを表示する
+    res.render('mainMenu', { title : 'mainMenu' });
 });
 
+/* ErrorPage */
 router.get('/failure', (req, res) => {
     console.log(req.session);
-    res.render('index', { title : 'TopPage' , message: 'ID PASSWORD が間違っています'});
+    res.render('index', { title : 'TopPage' , message: 'ID or PASSWORD が間違っています'});
 });
 
 /* 認証処理(POST) */
@@ -28,7 +46,7 @@ router.post('/',passport.authenticate('local',
             //await myServer.writeLog(req.user.username, "ログインしました。");
 
             //メインメニューへ遷移
-            res.render('mainMenu222', { title : 'mainMenu222' });
+            res.render('mainMenu', { title : 'mainMenu' });
             //res.send('Success');
         }
 
--- routes/mainMenu.js
+++ routes/mainMenu.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('mainMenu', { title : 'mainMenu' });
+let isLogined = function(req, res, next){
+    console.log(req.user);
+    if(req.isAuthenticated()){
+        //認証されている場合
+        if (req.user.myKey == 'zzz') {
+            //ログインOKの場合(router.getの中が処理される)
+            return next();
+        } else {
+            //権限がない場合
+            res.redirect("/");
+        }
+    }else{
+        //認証されていない場合
+        res.redirect("/");
+    }
+};
+
+router.get('/', isLogined, function(req, res, next) {
+    //認証OKの場合に処理される
+    res.render('mainMenu', { title : 'mainMenu' });
 });
 
 module.exports = router;
